c281445228e5705df8ec628a656ce1be56ec0c0c
RenIchi <terence.gaffud@skillsunion.com>
2022-03-15 14:37:04 +0800

    Cleaned up code

--- assignment_code/Building.java
+++ assignment_code/Building.java
@@ -1,6 +1,5 @@
 package assignment_code;
 
 public interface Building {
-    abstract void addFloor(int floors);
-    abstract String describe();
+
 }
--- assignment_code/Student.java
+++ assignment_code/Student.java
@@ -1,29 +1,5 @@
 package assignment_code;
 
-public class Student extends Person{
-    private int yrLevel;
+public class Student{
 
-    public Student(int yrLevel) {
-        this.yrLevel = yrLevel;
-    }
-
-    public Student() {
-    }
-
-    public Student(String name, int age, String gender, int yrLevel){
-        super(name, age, gender);
-        this.yrLevel = yrLevel;
-    }
-
-    public int getYrLevel() {
-        return this.yrLevel;
-    }
-
-    public void setYrLevel(int yrLevel) {
-        this.yrLevel = yrLevel;
-    }
-
-    public void introduce(){
-        System.out.println("Hello, my name is " + this.name + ", and I am a level " + this.yrLevel + " student");
-    }
 }
